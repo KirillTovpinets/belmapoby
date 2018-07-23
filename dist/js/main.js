b74ead881f1b351bafdeb815509a050a20af04ca
Товпинец Кирилл <Tovpinec@mapo.by>
2018-07-23 16:52:22 +0300

    add files to repo

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        prevArrow: `<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>`,
+        nextArrow: `<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>`
+    })
+})
